119b0a4431904b7527366180dc43901dd1667823
Shobhan S <shobhan.11.2000@gmail.com>
2022-03-24 16:40:00 +0530

    Unit testing updated

---
+++ Checkdivisibilitybyseven.py
@@ -0,0 +1,19 @@
+import unittest
+def checkdiv(x):
+    if x%7==0:
+        return True
+    else:
+        return False
+class check7app(unittest.TestCase):
+    def test_case1(self):
+        x=14
+        r=checkdiv(x)
+        self.assertTrue(r)
+
+    def test_case2(self):
+        x=13
+        r=checkdiv(x)
+        self.assertFalse(r)
+
+if __name__=="__main__":
+    unittest.main()
